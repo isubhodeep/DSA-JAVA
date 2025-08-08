baa497f5f4d719dd394af72246c8e4fb9bd245b8
Subhodeep Halder <142721252+isubhodeep@users.noreply.github.com>
2025-08-08 18:03:14 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (61.33%) - LeetSync

---
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int left=0,right=0;
+    public int maxDepth(TreeNode root) {
+        if(root==null)
+        return 0;
+        int left=maxDepth(root.left);
+        int right=maxDepth(root.right);
+        if(left>right)
+        return 1+left;
+        else
+        return 1+right;
+    }
+}
