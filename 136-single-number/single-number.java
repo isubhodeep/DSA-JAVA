ee887d94d0bde01bd3e640a7084c729b2266f416
Subhodeep Halder <142721252+isubhodeep@users.noreply.github.com>
2025-08-14 00:30:43 +0530

    Time: 1 ms (99.84%) | Memory: 46.6 MB (10.70%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,13 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int x1=0, x2=0;
+        int x1=0;
         for(int i=0;i<nums.length;i++)
         {
-            if(i%2==0)
-            x2=x2^nums[i];
-            else
             x1=x1^nums[i];
         }
-        return (x1^x2);
+        return x1;
     }
 }
