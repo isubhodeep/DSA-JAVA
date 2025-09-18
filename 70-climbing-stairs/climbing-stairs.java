8df88378f51021ea33915246f14619055c94538d
Subhodeep Halder <142721252+isubhodeep@users.noreply.github.com>
2025-09-18 17:48:09 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (10.98%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,18 +1,24 @@
 class Solution {
     public int climbStairs(int n) {
         int arr[] = new int[n+1];
-
-        return logic(n, arr);
-    }
-    public int logic(int n, int arr[]){ //2
-        if(n==1) return 1;
-        if(n==0) return 1;
-        if(arr[n]!=0)
+        arr[0]=1;
+        arr[1]=1;
+        for(int i=2;i<=n;i++){
+        int L = arr[i-1];//2-1 =1
+        int R = arr[i-2];//2-2 =1
+        arr[i]=L+R;
+        }
         return arr[n];
-        int L = logic(n-1,arr);//2-1 =1
-        int R = logic(n-2,arr);//2-2 =1
-        return arr[n]=L+R;
     }
+    // public int logic(int n, int arr[]){ //2
+    //     if(n==1) return 1;
+    //     if(n==0) return 1;
+    //     if(arr[n]!=0)
+    //     return arr[n];
+    //     int L = logic(n-1,arr);//2-1 =1
+    //     int R = logic(n-2,arr);//2-2 =1
+    //     return arr[n]=L+R;
+    // }
 }
 // 4
 // arr[0,0,2,0,0] = initial
