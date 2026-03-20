c00c9658a6d4b925a79985d0aa3d80b37a0e8744
Subhodeep Halder <142721252+isubhodeep@users.noreply.github.com>
2026-03-20 19:20:07 +0530

    Time: 5 ms (8.99%) | Memory: 47.9 MB (51.78%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,6 +1,12 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int i=0, j=0;
+        for(i=0;i<nums.length;i++)
+        {
+            if(nums[i]==0)
+                break;
+        }
+        j=i+1;
         while(i<nums.length && j<nums.length)
         {
             if(nums[i]==0 && nums[j]!=0)
@@ -10,12 +16,12 @@ class Solution {
                 nums[i]=nums[i]^nums[j];
             }
             if(nums[i]!=0)
-            i++;
-            if(nums[j]==0)
+                i++;
             j++;
-            if(i>j)
-            j=i+1;
-            
         }
     }
 }
+// 0 1 0 3 12
+// i j
+// 1 0 0 3 12
+//   i
